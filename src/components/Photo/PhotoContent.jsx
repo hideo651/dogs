ac59c125fad8145c147077966926dc8031d4d8ba
hideo651 <hideo651@msn.com>
2024-03-27 11:00:50 -0300

    botao de deletar a propria imagem funcionando

--- src/components/Photo/PhotoContent.jsx
+++ src/components/Photo/PhotoContent.jsx
@@ -1,11 +1,16 @@
 /* eslint-disable react/prop-types */
 
+import React from "react";
 import styles from "./PhotoContent.module.css";
 import { Link } from "react-router-dom";
 import PhotoComments from "./PhotoComments";
+import { UserContext } from "../../UserContext";
+import PhotoDelete from "./PhotoDelete";
 
 const PhotoContent = ({ data }) => {
+  const user = React.useContext(UserContext);
   const { photo, comments } = data;
+
   return (
     <div className={styles.photo}>
       <div className={styles.img}>
@@ -13,10 +18,14 @@ const PhotoContent = ({ data }) => {
       </div>
       <div className={styles.details}>
         <div>
-          <p className={styles.author}>
-            <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
+          <div className={styles.author}>
+            {user.data && user.data.username === photo.author ? (
+              <PhotoDelete id={photo.id} />
+            ) : (
+              <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
+            )}
             <span className={styles.visualizacoes}>{photo.acessos}</span>
-          </p>
+          </div>
           <h1 className="title">
             <Link to={`/foto/${photo.id}`}>{photo.title}</Link>
           </h1>
