e6804d1e27bcdc6efccc4d2dd221b1530c539709
hideo651 <hideo651@msn.com>
2024-04-05 15:32:22 -0300

    caminho dos componentes arrumado

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "./Components/Header";
-import Footer from "./Components/Footer";
-import Home from "./Components/Home";
-import Login from "./Components/Login/Login";
+
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+import Login from "./components/Login/Login";
 import { UserStorage } from "./UserContext";
-import User from "./Components/User/User";
-import ProtectedRoute from "./Components/Helper/ProtectedRoute";
-import Photo from "./Components/Photo/Photo";
-import UserProfile from "./Components/User/UserProfile";
-import NotFound from "./Components/NotFound";
+import User from "./components/User/User";
+import ProtectedRoute from "./components/Helper/ProtectedRoute";
+import Photo from "./components/Photo/Photo";
+import UserProfile from "./components/User/UserProfile";
+import NotFound from "./components/NotFound";
+import Header from "./components/Header";
 
 function App() {
   return (
